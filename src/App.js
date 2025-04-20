fc1c65f290741773e8ef7fe9cb50667cfb1a033b
Chrystal Lim Jing Yee <chrystal.limjy@gmail.com>
2025-04-21 00:06:35 +0800

    commit partial data for ttt
    partial tutorial complete for tic tact toe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/> {/*forgot to include this, as such, the TicTacToe never apppearned */}
+    </div>
+  );
+}
+
+export default App;
